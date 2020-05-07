6dd533667b17d9219c7e498400c2b47075f7487e
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-05-07 14:26:08 -0400

    reset config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     PORT: process.env.PORT || 8000,
-    // API_BASE_URL: 'https://canonize-server.herokuapp.com'
-    API_BASE_URL: 'http://localhost:8000'
+    API_BASE_URL: 'https://canonize-server.herokuapp.com'
+    // API_BASE_URL: 'http://localhost:8000'
 };
